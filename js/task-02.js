c716309eef9ebc7739c159579655d3c5e16a3f76
olga-plotkina <olga.semenyuta@gmail.com>
2022-07-26 18:59:02 +0100

    updates task-2

--- js/task-02.js
+++ js/task-02.js
@@ -9,19 +9,39 @@ const ingredients = [
 
 const listRef = document.querySelector('#ingredients');
 
+// Option 1
 // for (const ingredient of ingredients) {
 //   const itemRef = document.createElement('li');
 //   itemRef.textContent = ingredient;
 //   itemRef.classList.add('item');
 //   listRef.appendChild(itemRef);
 // }
-const itemArray = '';
-for (const ingredient of ingredients) {
-  const itemRef = document.createElement('li');
-  itemRef.textContent = ingredient;
-  itemRef.classList.add('item');
-  itemArray += itemRef;
+
+// Option 2
+
+// const itemArray = [];
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const itemRef = document.createElement('li');
+//   itemRef.textContent = ingredients[i];
+//   itemRef.classList.add('item');
+//   itemArray.push(itemRef);
+// }
+
+// console.log(itemArray);
+// listRef.append(...itemArray);
+
+// Option 3 
+
+const makeListOfItems = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemRef = document.createElement('li');
+    itemRef.textContent = ingredient;
+    itemRef.classList.add('item');
+    
+    return itemRef;
+})
 }
 
-console.log(itemArray);
-listRef.append(itemArray);
+const itemsFromIngredients = makeListOfItems(ingredients);
+listRef.append(...itemsFromIngredients);
