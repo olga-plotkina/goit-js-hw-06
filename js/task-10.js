706a9fcff0f4650b65f9ec1905d9d31532c40710
olga-plotkina <olga.semenyuta@gmail.com>
2022-07-29 01:34:54 +0100

    updated task10

--- js/task-10.js
+++ js/task-10.js
@@ -2,45 +2,10 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// const images = [
-//   {
-//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'White and Black Long Fur Cat',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
-// ];
-
-// const listOfGalleryRef = document.querySelector('.gallery');
-
-// const makeGalleryMarkup = ({url, alt}) => {
-//   return `
-//   <li><img 
-//   width=`${url}px`
-//   height=`${alt}px``>
-//   </li>
-//   `;
-// };
-
-// const makeGallery = images
-// .map(makeGalleryMarkup)
-// .join('');
-
-// listOfGalleryRef.innerHTML = makeGallery;
-
-
-
 const buttonCreateRef = document.querySelector('button[data-create]');
 const buttonDestroyRef = document.querySelector('button[data-destroy]');
 const inputRef = document.querySelector('input');
 const divRef = document.querySelector('#boxes');
-
 
 
 inputRef.addEventListener('change', (event) => {
@@ -51,11 +16,8 @@ inputRef.addEventListener('change', (event) => {
 
   buttonDestroyRef.addEventListener('click', () => {
     destroyBoxes();
-    event.currentTarget.elements.reset();
   })
 });
-
-
 
 
 function createBoxes(amount) {
@@ -69,9 +31,10 @@ function createBoxes(amount) {
     break;
   }
   const result = arrayMarkup.join('');
-  return divRef.innerHTML = result;
+  divRef.innerHTML = result;
 }
 
 function destroyBoxes() {
   divRef.innerHTML = '';
+  inputRef.value = '';
 };
