f59e87eadaae230601a477216d6bc92d076011c9
olga-plotkina <olga.semenyuta@gmail.com>
2022-07-30 14:49:54 +0100

    fixed task 2

--- js/task-02.js
+++ js/task-02.js
@@ -9,39 +9,13 @@ const ingredients = [
 
 const listRef = document.querySelector('#ingredients');
 
-// Option 1
-// for (const ingredient of ingredients) {
-//   const itemRef = document.createElement('li');
-//   itemRef.textContent = ingredient;
-//   itemRef.classList.add('item');
-//   listRef.appendChild(itemRef);
-// }
 
-// Option 2
+const itemsFromIngredients = ingredients.map(ingredient => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = ingredient;
+  itemRef.classList.add('item');
+  
+  return itemRef;
+});
 
-// const itemArray = [];
-
-// for (let i = 0; i < ingredients.length; i += 1) {
-//   const itemRef = document.createElement('li');
-//   itemRef.textContent = ingredients[i];
-//   itemRef.classList.add('item');
-//   itemArray.push(itemRef);
-// }
-
-// console.log(itemArray);
-// listRef.append(...itemArray);
-
-// Option 3 
-
-const makeListOfItems = ingredients => {
-  return ingredients.map(ingredient => {
-    const itemRef = document.createElement('li');
-    itemRef.textContent = ingredient;
-    itemRef.classList.add('item');
-    
-    return itemRef;
-})
-}
-
-const itemsFromIngredients = makeListOfItems(ingredients);
 listRef.append(...itemsFromIngredients);
